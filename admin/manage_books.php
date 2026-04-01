93bfa10e62ee7297ef5b5936b614c544c4311f85
Mithesha Nuwanjana <nmithesha@gmail.com>
2026-04-01 15:13:42 +0530

    Create manage_books.php

---
+++ admin/manage_books.php
@@ -0,0 +1,5 @@
+<?php 
+// php code to manage books  goes here
+//idea:A large HTML table that lists every single book currently in the library. Next to every book in that table, there are usually two action buttons: "Edit" and "Delete"
+//The central control room for the inventory. It is not a single action, but rather a dashboard or list view
+?>
