ac0ec1e996e02b7eeb1f79d5136f0fc0c7f6bf26
MNuwanjana <nmithesha@gmail.com>
2026-04-02 23:21:21 +0530

    Add homepage content to index.php with header and footer includes

--- index.php
+++ index.php
@@ -1,3 +1,8 @@
-<?php 
-// homepage code goes here 
-?>
+<?php include 'includes/header.php'; ?>
+
+<div class="p-5 text-center bg-white rounded shadow-sm">
+    <h1 class="display-4">Welcome to OLMS!</h1>
+    <p class="lead">The easiest way to manage your library.</p>
+</div>
+
+<?php include 'includes/footer.php'; ?>
